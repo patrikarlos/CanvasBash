091fc5bc3b8439cc863460501f8b3ebf45a96b67
Patrik Arlos <Patrik.Arlos@bth.se>
2022-12-18 17:28:50 +0100

    Clarified output.

--- remainToReview.sh
+++ remainToReview.sh
@@ -53,5 +53,5 @@ if [[ "$GRADED" == "1" ]]; then
     echo "$grades"
 else
     
-    echo "$graded $reviewed/$assignments"
+    echo "$graded $reviewed/$assignments   (Graded Reviewed/Assignments)"
 fi
